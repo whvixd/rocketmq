f484ff64b55e7491b7feee1e6e276a4089e8c6da
wangzhixiang <wangzhixiang_v@didiglobal.com>
2021-06-20 17:07:51 +0800

    RouteInfoManager参数含义

--- namesrv/src/main/java/org/apache/rocketmq/namesrv/routeinfo/RouteInfoManager.java
+++ namesrv/src/main/java/org/apache/rocketmq/namesrv/routeinfo/RouteInfoManager.java
@@ -49,9 +49,9 @@ public class RouteInfoManager {
     private static final InternalLogger log = InternalLoggerFactory.getLogger(LoggerName.NAMESRV_LOGGER_NAME);
     private final static long BROKER_CHANNEL_EXPIRED_TIME = 1000 * 60 * 2; // 两分钟
     private final ReadWriteLock lock = new ReentrantReadWriteLock();
-    private final HashMap<String/* topic */, List<QueueData>> topicQueueTable;
-    private final HashMap<String/* brokerName */, BrokerData> brokerAddrTable;
-    private final HashMap<String/* clusterName */, Set<String/* brokerName */>> clusterAddrTable;
+    private final HashMap<String/* topic */, List<QueueData>> topicQueueTable; // topic数据信息
+    private final HashMap<String/* brokerName */, BrokerData> brokerAddrTable; // broker数据信息
+    private final HashMap<String/* clusterName */, Set<String/* brokerName */>> clusterAddrTable; // broker机器名称对应broker名称（见broker.conf）
     private final HashMap<String/* brokerAddr */, BrokerLiveInfo> brokerLiveTable;
     private final HashMap<String/* brokerAddr */, List<String>/* Filter Server */> filterServerTable;
 
